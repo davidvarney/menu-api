5e311e6fc6eee8d5c09976377929ca376186e12d
David Varney <davidvarney@gmail.com>
2017-02-04 15:01:32 -0500

    Adds some updates for the pivot model for order items and extras.

--- app/models/extra.rb
+++ app/models/extra.rb
@@ -1,2 +1,4 @@
 class Extra < ActiveRecord::Base
+  has_many :order_item_extras
+  has_many :order_items, :through => :order_item_extras
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,6 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :item
   belongs_to :size
+  has_many :order_item_extras
+  has_many :extras, :through => :order_item_extras
 end
