0d08cd3d3890b1b2ca27c861c00242f0c5d2f509
David Varney <davidvarney@gmail.com>
2017-02-05 13:21:30 -0500

    Removes the modification to the item type serializer we implemented last night.

--- app/serializers/item_type_serializer.rb
+++ app/serializers/item_type_serializer.rb
@@ -1,9 +1,9 @@
 class ItemTypeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :items
+  attributes :id, :name#, :items
 
-  def items
-    object.items.map do |item|
-      ItemSerializer.new(item, scope: scope, root: false, event: object)
-    end
-  end
+  #def items
+  #  object.items.map do |item|
+  #    ItemSerializer.new(item, scope: scope, root: false, event: object)
+  #  end
+  #end
 end
